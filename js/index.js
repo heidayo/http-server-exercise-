160fb1f61435f3d72eccadb51f2d296d13554e0c
heidayo <h.chiba@makeculture.jp>
2026-01-12 03:12:14 +0000

    index.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+<script>
+    function clickHandler() {
+            const box = document.getElementById("box");
+            const p = document.createElement("p");
+            p.innerText = "Added by JS";
+            box.appendChild(p);
+    }
+    function clickHandler2() {
+            const box = document.getElementById("box");
+            const p = document.createElement("p");
+            p.innerText = "Added by JS2";
+            box.appendChild(p);
+    }
+</script>
